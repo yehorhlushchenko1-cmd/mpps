a955c67cb8811ccea3bef3632f8576f402913e66
Egor Gluschenko <egor.glushchenko147@gmail.com>
2026-05-16 18:13:41 +0300

    MPPS-1 Test commit

--- MPPS/Class1.cs
+++ MPPS/Class1.cs
@@ -8,5 +8,11 @@ namespace MPPS
 {
     internal class Class1
     {
+        public static void Main()
+        {
+            int a = 1, b = 2, c = 3;
+            Console.WriteLine(a);
+            Console.ReadKey(true);
+        }
     }
 }
